69601792f4f3d623ccd6d2d2e83dee49119b3a3e
Pavel Kurianov <kurianov.pavel@gmail.com>
2019-11-09 21:52:29 +0200

    Add converters update project links

---
+++ XamBasePacket/Converters/BoolToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamBasePacket.Converters
+{
+    public class BoolToStringConverter : IValueConverter
+    {
+        public string TrueValue { get; set; }
+        public string FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return FalseValue;
+            if (value is bool val)
+            {
+                return val ? TrueValue : FalseValue;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ XamBasePacket/Converters/NullToBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamBasePacket.Converters
+{
+    public class NullToBoolConverter : IValueConverter
+    {
+        public bool WhenNull { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+                return string.IsNullOrEmpty(str) ? WhenNull : !WhenNull;
+            return value == null ? WhenNull : !WhenNull;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
